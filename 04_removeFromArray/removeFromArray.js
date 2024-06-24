9fed05ce242ee47b9d5ad12db30c3bb5811724cb
bharajuice <aneeb.02@gmail.com>
2024-06-24 20:21:59 +0500

    updated 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...val) {
+  let filter = [];
+  array.forEach((item) => {
+    if(!val.includes(item))
+      filter.push(item);
+  })
+  // for(let i = 0; i< array.length; i++){
+  //   if(array[i] != val)
+  //     filter.push(array[i]);
+  // } 
+  return filter;
 };
 
 // Do not edit below this line
